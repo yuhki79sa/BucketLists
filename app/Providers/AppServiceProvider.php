47ab78377eb451b6e20ecc65335c881989d93933
yuhki79sa <yuhki79sa@gmail.com>
2025-05-31 13:56:25 +0900

    Procfile for Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
+     public function boot(): void
+     {
+     \URL::forceScheme('https');
+     $this->app['request']->server->set('HTTPS','on');
+     }
 }
