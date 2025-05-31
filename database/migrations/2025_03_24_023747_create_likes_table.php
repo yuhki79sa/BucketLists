803695f62694a3cf8f8b7d898a34ce2830723bf6
yuhki79sa <yuhki79sa@gmail.com>
2025-05-31 21:30:54 +0900

    fix

--- database/migrations/2025_03_24_023747_create_likes_table.php
+++ database/migrations/2025_03_24_023747_create_likes_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('likes', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->primary(['user_id', 'post_id']);
             $table->timestamps();
         });
     }
